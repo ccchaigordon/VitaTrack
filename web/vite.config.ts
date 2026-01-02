7181e0d530ff9f2ea77cb39f5e9a00a3f31d91f7
Gordon Chai <chaigordon75@gmail.com>
2026-01-03 00:15:31 +0800

    PWA fixes #3

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -22,13 +22,13 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/logo.png',
+            src: '/vita.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: '/logo.png',
+            src: '/vita.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
